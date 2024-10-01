50c08e7f5f0d5be0c2a1d1f873d580aaaeca0fd0
Lev Fomenko <Lev.Fomenko@intelli-vision.com>
2024-10-01 22:54:13 +0500

    add back service skeleton. p2

--- back/appointment-service/internal/storage/storage.go
+++ back/appointment-service/internal/storage/storage.go
@@ -1,6 +1,7 @@
 package storage
 
 import (
+	"fmt"
 	types "scheduler/appointment-service/internal"
 	"time"
 
@@ -9,20 +10,21 @@ import (
 
 type dbSlot struct {
 	Client string `db:"client_id"`
-	Date   int64  `db:"date_unx"`
-	Len    int    `db:"len_sec"`
+	//	Business string `db:"business_id"`
+	Date int64 `db:"date_unx"`
+	Len  int   `db:"len_sec"`
 }
 
 const createAppointmentsTable = `CREATE TABLE "appointments" (
-	"date_unx"	INTEGER NOT NULL,
-	"business_id"	TEXT NOT NULL,
-	"client_id"	TEXT NOT NULL,
-	"len_sec"	INTEGER NOT NULL
+	"date_unx"	  INTEGER NOT NULL,
+	"business_id" TEXT NOT NULL,
+	"client_id"	  TEXT NOT NULL,
+	"len_sec"	  INTEGER NOT NULL CHECK(len_sec >= 5),
 );`
 
 func GetSlotInRange(db *sqlx.DB, business_id types.ID, start time.Time, end time.Time) ([]types.Slot, error) {
 	var dbSlots []dbSlot
-	err := db.Select(&dbSlots, "SELECT * FROM appointments WHERE business_id = ? AND date_unx BETWEEN ? AND ?", string(business_id), start.Unix(), end.Unix())
+	err := db.Select(&dbSlots, "SELECT * FROM appointments WHERE business_id = $1 AND date_unx BETWEEN $2 AND $3", string(business_id), start.Unix(), end.Unix())
 	if err != nil {
 		return nil, err
 	}
@@ -34,4 +36,16 @@ func GetSlotInRange(db *sqlx.DB, business_id types.ID, start time.Time, end time
 			Len:   slot.Len})
 	}
 	return slotsOut, nil
+}
+
+func AddSlots(db *sqlx.DB, appointment types.Appointment) error {
+	dbSlots := make([]dbSlot, 0, len(appointment.Slots))
+	for _, slot := range appointment.Slots {
+		//dbSlots = append(dbSlots, dbSlot{Client: string(slot.Client), Business: string(appointment.Business), Date: slot.Start.Unix(), Len: slot.Len})
+		dbSlots = append(dbSlots, dbSlot{Client: string(slot.Client), Date: slot.Start.Unix(), Len: slot.Len})
+	}
+
+	q := fmt.Sprintf("INSERT INTO appointments (date_unx, business_id, client_id, len_sec) VALUES (:date_unx, %s, :client_id, :len_sec)", appointment.Business)
+	_, err := db.NamedExec(q, dbSlots)
+	return err
 }
