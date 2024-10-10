acbc57985c57b44c4b5d5d61731c32811a0abd2e
Lev Fomenko <flevvi@gmail.com>
2024-10-10 22:41:19 +0500

    Time slot generation. p2

--- back/appointment-service/internal/storage/avalilable_slots.go
+++ back/appointment-service/internal/storage/avalilable_slots.go
@@ -3,11 +3,13 @@ package storage
 import (
 	"time"
 
+	common "scheduler/appointment-service/internal"
+
 	"github.com/teambition/rrule-go"
 )
 
 const countLimit = 100
-const slotIntervalMin = 15
+const slotIntervalMin = 15 // Hardcoded. Fix it by reading from db.
 
 // TODO add days off
 // Note "start" and "until" includes in the range
@@ -54,7 +56,62 @@ func generateTimeSlotsForDay(dtStart time.Time, duration time.Duration) ([]time.
 	return r.All(), nil
 }
 
-// func GetAvailableSlotsInRange(db *Storage, business_id common.ID, start time.Time, end time.Time) ([]common.Slot, error) {
-// 	var slots []common.Slot
+func (*Storage) GetPossibleSlotsInRange(business_id common.ID, start time.Time, end time.Time) ([]common.Slot, error) {
+	start = start.Truncate(time.Hour * 24).Add(time.Hour * 9) //TODO Fix it. hardcoded to 9am
+	const workingHours = time.Hour * 8                        // TODO Fix it. Hardcoded 8h
+	weekdays, err := generateWeekdays(start, end)
 
-// }
+	if err != nil {
+		return nil, err
+	}
+
+	var times []time.Time
+	for _, weekday := range weekdays {
+		tmp, err := generateTimeSlotsForDay(weekday, workingHours)
+		if err != nil {
+			return nil, err
+		}
+		times = append(times, tmp...)
+	}
+
+	out := make([]common.Slot, len(times))
+	for inx, t := range times {
+		out[inx] = common.Slot{Start: t, Len: slotIntervalMin}
+	}
+
+	return out, nil
+}
+
+// TODO not optimal. Result is unordered
+func (db *Storage) GetAvailableSlotsInRange(business_id common.ID, start time.Time, end time.Time) ([]common.Slot, error) {
+	slots, err := db.GetPossibleSlotsInRange(business_id, start, end)
+	if err != nil {
+		return nil, err
+	}
+
+	//TODO check for time order
+	busySlots, err := db.GetBusySlotsInRange(business_id, start, end)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(busySlots) == 0 {
+		return slots, nil
+	}
+
+	availableSlotsMap := make(map[int64]common.Slot, len(slots))
+	for _, slot := range slots {
+		availableSlotsMap[slot.Start.Unix()] = slot
+	}
+
+	for _, slot := range busySlots {
+		delete(availableSlotsMap, slot.Start.Unix())
+	}
+
+	availableSlots := slots[:len(availableSlotsMap)]
+	for inx, slot := range availableSlotsMap {
+		availableSlots[inx] = slot
+	}
+
+	return availableSlots, nil
+}
