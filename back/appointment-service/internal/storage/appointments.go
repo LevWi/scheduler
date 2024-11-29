2ba33df9c63dde1ef6dcf488dc30c91a37df11d4
Lev Fomenko <flevvi@gmail.com>
2024-11-30 02:50:21 +0500

    Interval rules db methods

--- back/appointment-service/internal/storage/appointments.go
+++ back/appointment-service/internal/storage/appointments.go
@@ -1,7 +1,10 @@
 package storage
 
 import (
+	"errors"
+	"math/rand/v2"
 	common "scheduler/appointment-service/internal"
+	"slices"
 	"time"
 
 	"github.com/jmoiron/sqlx"
@@ -18,6 +21,13 @@ type dbSlot struct {
 	DateEnd   int64  `db:"date_end"`
 }
 
+type DBRule = string
+
+type DbBusinessRule struct {
+	Id   int
+	Rule DBRule
+}
+
 const queryCreateAppointmentsTable = `CREATE TABLE "appointments" (
 	"date_start"	  INTEGER NOT NULL,
 	"date_end"	  INTEGER NOT NULL,
@@ -26,12 +36,74 @@ const queryCreateAppointmentsTable = `CREATE TABLE "appointments" (
 	UNIQUE (business_id, date_start)
 );`
 
+const createBusinessTable = `CREATE TABLE "business_work_rule" (
+	"id" INTEGER NOT NULL,
+	"business_id" TEXT NOT NULL,
+	"rule"	  TEXT NOT NULL,
+	UNIQUE (id, business_id)
+);`
+
 func CreateTableAppointments(db *Storage) error {
 	_, err := db.Exec(queryCreateAppointmentsTable)
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+func CreateBusinessTable(db *Storage) error {
+	_, err := db.Exec(createBusinessTable)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (db *Storage) GetBusinessRules(business_id common.ID) ([]DbBusinessRule, error) {
+	var rules []DbBusinessRule
+	err := db.Select(&rules, "SELECT id, rule FROM business_work_rule WHERE business_id = $1",
+		string(business_id))
+	if err != nil {
+		return nil, err
+	}
+
+	return rules, nil
+}
+
+func (db *Storage) AddBusinessRule(business_id common.ID, rule DBRule) error {
+	// Expected small count of rules for each business
+	var ids []int
+	err := db.Select(&ids, "SELECT id FROM business_work_rule WHERE business_id = $1",
+		string(business_id))
+	if err != nil {
+		return err
+	}
+
+	slices.Sort(ids)
+
+	idFound := false
+	newId := 0
+	for range 3 {
+		newId = rand.Int()
+		_, idFound = slices.BinarySearch(ids, newId)
+		if !idFound {
+			break
+		}
+	}
+
+	if idFound {
+		return errors.New("BusinessRule: Not found unique id. Try again")
+	}
+
+	_, err = db.Exec("INSERT INTO business_work_rule (id, business_id, rule) VALUES ($1, $2, $3)", newId, business_id, rule)
+	return err
+}
+
+// TODO is value deletion confirm needed?
+func (db *Storage) DeleteBusinessRule(business_id common.ID, id int) error {
+	_, err := db.Exec("DELETE FROM business_work_rule WHERE business_id = $1 AND id = $2",
+		business_id, id)
+	return err
 }
 
 // func (db *Storage) GetAvailableSlotsInRange(business_id common.ID, start time.Time, end time.Time) ([]common.Slot, error) {
--- back/appointment-service/internal/storage/storage_test.go
+++ back/appointment-service/internal/storage/storage_test.go
@@ -3,6 +3,8 @@ package storage
 import (
 	"fmt"
 	"os"
+	"slices"
+	"strconv"
 	"testing"
 	"time"
 
@@ -13,7 +15,7 @@ import (
 	types "scheduler/appointment-service/internal"
 )
 
-func TestStorage(t *testing.T) {
+func initDB(t *testing.T) Storage {
 	dbPath := t.TempDir() + string(os.PathSeparator) + "test_.db"
 	t.Log("db path:", dbPath)
 
@@ -21,11 +23,14 @@ func TestStorage(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer db.Close()
+	return Storage{DB: db}
+}
 
-	storage := Storage{DB: db}
+func TestStorage(t *testing.T) {
+	storage := initDB(t)
+	defer storage.Close()
 
-	err = CreateTableAppointments(&storage)
+	err := CreateTableAppointments(&storage)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -82,5 +87,64 @@ func TestStorage(t *testing.T) {
 	if appointment.Slots[1].Start != slots[0].Start || appointment.Slots[1].End != slots[0].End || appointment.Slots[1].Client != slots[0].Client {
 		fmt.Printf("slot mismatch : %+v != %+v", slots[0], appointment.Slots[1])
 		t.FailNow()
+	}
+}
+
+func TestStorageBusinessRule(t *testing.T) {
+	storage := initDB(t)
+	defer storage.Close()
+
+	err := CreateBusinessTable(&storage)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	const businessName = "Business Name"
+	const rulesCount = 100
+	for x := range rulesCount {
+		err = storage.AddBusinessRule(businessName, "some_rule"+strconv.Itoa(x))
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	var count int
+	err = storage.Get(&count, "SELECT COUNT(*) FROM business_work_rule WHERE rule = \"some_rule42\"")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if count != 1 {
+		t.Fatal("unexpect count: ", count)
+	}
+
+	rules, err := storage.GetBusinessRules(businessName)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(rules) != rulesCount {
+		t.Fatal("unexpect rules count: ", len(rules))
+	}
+
+	for i, r := range rules {
+		if r.Rule != "some_rule"+strconv.Itoa(i) {
+			t.Fatal("unexpect rule: ", r.Rule)
+		}
+	}
+
+	err = storage.DeleteBusinessRule(businessName, rules[42].Id)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rules, err = storage.GetBusinessRules(businessName)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(rules) != rulesCount-1 {
+		t.Fatal("unexpect rules count: ", len(rules))
+	}
+
+	if slices.ContainsFunc(rules, func(r DbBusinessRule) bool { return r.Rule == "some_rule42" }) {
+		t.Fatal("rule should be deleted")
 	}
 }
