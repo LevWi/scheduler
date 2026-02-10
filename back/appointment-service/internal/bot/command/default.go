7b421f9d3f9cdf90c430b4fe70f6059a4f943980
Lev Fomenko <flevvi@gmail.com>
2026-02-10 17:46:22 +0500

    Draft. p12. Http requests refactoring

--- back/appointment-service/internal/bot/command/default.go
+++ back/appointment-service/internal/bot/command/default.go
@@ -1,22 +1,20 @@
 package command
 
 import (
-	common "scheduler/appointment-service/internal"
 	"scheduler/appointment-service/internal/bot"
 	"scheduler/appointment-service/internal/bot/chat"
 	"scheduler/appointment-service/internal/bot/i18n/messages"
 )
 
-func NewDefaultMainMenu(chat chat.Chat, l *messages.Localization, businessID common.ID, connection *bot.SchedulerConnection) (*MainMenu, error) {
+func NewDefaultMainMenu(chat chat.Chat, l *messages.Localization, connection *bot.SchedulerConnection) (*MainMenu, error) {
 	md, err := NewMenuDeps(chat, l)
 	if err != nil {
 		return nil, err
 	}
-	return newMainMenu(md, newDefaultSlotSelectionCommand(md, businessID, connection)), nil
+	return newMainMenu(md, newDefaultSlotSelectionCommand(md, connection)), nil
 }
 
-func newDefaultSlotSelectionCommand(md *MenuDeps, businessID common.ID, connection *bot.SchedulerConnection) *SlotSelectionCommand {
-	return newSlotSelectionCommand(md,
-		NewWeekSlots(&HttpSlotsProvider{baseURL: connection.URL, businessID: businessID}),
-		&HttpAppointment{Connection: connection})
+func newDefaultSlotSelectionCommand(md *MenuDeps, connection *bot.SchedulerConnection) *SlotSelectionCommand {
+	ha := &HttpAppointment{Connection: connection}
+	return newSlotSelectionCommand(md, NewWeekSlots(ha), ha)
 }
--- back/appointment-service/internal/bot/command/http_appointment.go
+++ back/appointment-service/internal/bot/command/http_appointment.go
@@ -54,3 +54,47 @@ func (a *HttpAppointment) AddSlots(ctx context.Context, customer common.ID, slot
 
 	return checkStatusCode(resp)
 }
+
+// TODO make function swagger.Slot -> common.Slot
+func (p *HttpAppointment) AvailableSlotsInRange(ctx context.Context, interval common.Interval) ([]common.Slot, error) {
+	u, err := url.JoinPath(p.Connection.URL, "slots", p.Connection.BusinessID)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequestWithContext(ctx, "GET", u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	v := req.URL.Query()
+	v.Set("date_start", interval.Start.Format(time.RFC3339))
+	v.Set("date_end", interval.End.Format(time.RFC3339))
+	req.URL.RawQuery = v.Encode()
+
+	//TODO with timeout?
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	err = checkStatusCode(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	//TODO print request id in log
+	var slots swagger.AvailableSlots
+	err = json.NewDecoder(resp.Body).Decode(&slots)
+	if err != nil {
+		return nil, fmt.Errorf("http: unexpected response (%s)", resp.Status)
+	}
+
+	out := make([]common.Slot, 0, len(slots.Slots))
+	for _, slot := range slots.Slots {
+		var tmp common.Slot
+		tmp.Start = slot.TpStart
+		tmp.Dur = time.Minute * time.Duration(slot.Len)
+		out = append(out, tmp)
+	}
+	return out, nil
+}
