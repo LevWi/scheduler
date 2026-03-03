8b398ae08f619579d32f2c7f223d96ad8d291d11
Lev Fomenko <flevvi@gmail.com>
2026-03-03 21:09:29 +0500

    Draft. p19. Demo fix

--- back/appointment-service/cmd/utils/test_db_filler/main.go
+++ back/appointment-service/cmd/utils/test_db_filler/main.go
@@ -1,21 +1,56 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
+	"runtime"
+	common "scheduler/appointment-service/internal"
 	"scheduler/appointment-service/internal/dbase/auth"
+	"scheduler/appointment-service/internal/dbase/backend/slots"
 	"scheduler/appointment-service/internal/dbase/bots"
 	"scheduler/appointment-service/internal/dbase/test"
+
+	"github.com/teambition/rrule-go"
 )
+
+const UnknownFn = "<fn unknown>"
+
+func CallerLine(skip int) int {
+	_, _, line, ok := runtime.Caller(skip + 1)
+	if !ok {
+		return -1
+	}
+	return line
+}
 
 func PrintVar(name string, value any) {
 	fmt.Printf("%-10s = %#v\n", name, value)
 }
 
 func Fatal(msg any) {
+	_, _, line, ok := runtime.Caller(1)
+	if !ok {
+		line = -1
+	}
+	fmt.Println("L:", line)
 	fmt.Println(msg)
 	os.Exit(1)
+}
+
+func createRule() common.IntervalRRule {
+	const rruleStr = "DTSTART=20260101T090000Z;FREQ=DAILY;COUNT=1000"
+	var rule common.IntervalRRule
+	{
+		r, e := rrule.StrToRRule(rruleStr)
+		if e != nil {
+			Fatal(e)
+		}
+		rule.RRule = r
+		rule.Len = 60 * 60 * 8
+	}
+	return rule
 }
 
 func main() {
@@ -54,6 +89,23 @@ func main() {
 		Fatal(err)
 	}
 
+	slts := slots.TimeSlotsStorage{DB: db}
+
+	rule := common.IntervalRRuleWithType{
+		Rule: createRule(),
+		Type: common.Inclusion,
+	}
+
+	strRule, err := json.Marshal(rule)
+	if err != nil {
+		Fatal(err)
+	}
+
+	err = slts.AddBusinessRule(userId, string(strRule))
+	if err != nil {
+		Fatal(err)
+	}
+
 	PrintVar("dbPath", dbPath)
 	PrintVar("user", user)
 	PrintVar("pass", pass)
@@ -61,7 +113,5 @@ func main() {
 	PrintVar("botID", botID)
 	PrintVar("botToken", botToken)
 	PrintVar("botInfo", botInfo)
-
-	//slots.TimeSlotsStorage{test.InitTmpDB(t)}
-
+	PrintVar("Rule", string(strRule))
 }
