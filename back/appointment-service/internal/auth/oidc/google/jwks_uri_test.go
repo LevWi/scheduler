d53d2dccbcd65afb051f13bdaf4af43a58c84617
Lev Fomenko <flevvi@gmail.com>
2026-03-11 00:45:27 +0500

    Stabilize Google JWK URI test by mocking OpenID endpoint

--- back/appointment-service/internal/auth/oidc/google/jwks_uri_test.go
+++ back/appointment-service/internal/auth/oidc/google/jwks_uri_test.go
@@ -2,14 +2,34 @@ package google
 
 import (
 	"context"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestFetchGoogleJWKsUri(t *testing.T) {
 	const expected = "https://www.googleapis.com/oauth2/v3/certs"
-	uri, err := FetchGoogleJWKsUri(context.Background())
-	assert.NoError(t, err)
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte(`{"jwks_uri":"` + expected + `"}`))
+	}))
+	defer ts.Close()
+
+	uri, err := fetchJWKsURI(context.Background(), ts.URL, ts.Client())
+	require.NoError(t, err)
 	assert.Equal(t, expected, uri)
+}
+
+func TestFetchGoogleJWKsUri_ErrorStatus(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusForbidden)
+	}))
+	defer ts.Close()
+
+	_, err := fetchJWKsURI(context.Background(), ts.URL, ts.Client())
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), "unexpected status code")
 }
